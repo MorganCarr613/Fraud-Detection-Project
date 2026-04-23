28dd5e4ba42ffb5018889d5e56457eef354c5923
MorganCarr613 <morgan.e.carr@tcu.edu>
2026-04-23 12:36:13 -0500

    Update StreamlitApp_HW_5.py

--- Portfolio/StreamlitApp_HW_5.py
+++ Portfolio/StreamlitApp_HW_5.py
@@ -57,8 +57,8 @@ MODEL_INFO = {
         "endpoint": aws_endpoint,
         "explainer": 'explainer_project.shap', 
         "pipeline": 'fine_tuned_gbm_pipeline.tar.gz', 
-        "keys": ["num_C1","num_D1"],   # change these to match the names of the two most important feature 
-        "inputs": [{"name": k, "type": "number", "min": -100.0, "max": 100.0, "default": 0.0, "step": 10.0} for k in ["num_C1","num_D1"]]} 
+        "keys": ["num_C1","num_D8"],   # change these to match the names of the two most important feature 
+        "inputs": [{"name": k, "type": "number", "min": -100.0, "max": 100.0, "default": 0.0, "step": 10.0} for k in ["num_C1","num_D8"]]} 
 
 
 def load_pipeline(_session, bucket, key):
