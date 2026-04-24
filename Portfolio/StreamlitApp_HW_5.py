46bbbd092136a94c694243a48f6de5157ba9106a
MorganCarr613 <morgan.e.carr@tcu.edu>
2026-04-23 20:38:15 -0500

    Update StreamlitApp_HW_5.py

--- Portfolio/StreamlitApp_HW_5.py
+++ Portfolio/StreamlitApp_HW_5.py
@@ -70,8 +70,8 @@ MODEL_INFO = {
     "endpoint"  : aws_endpoint,
     "explainer" : "explainer_project.shap",
     "pipeline"  : "fine_tuned_gbm_pipeline.tar.gz",
-    "keys"      : ['num_D2','num_C2','num_C1'],
-    "inputs"    : [{"name": k, "type": "number", "min": -1.0, "max": 1.0, "default": 0.0, "step": 0.01} for k in ['TransactionAmt','addr1','addr2']]
+    "keys"      : ['num_C1','num_C10','cat_card6_credit'],
+    "inputs"    : [{"name": k, "type": "number", "min": -1.0, "max": 1.0, "default": 0.0, "step": 0.01} for k in ['num_C1','num_C10','cat_card6_credit']]
 }
 
 
